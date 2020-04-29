538ebe4a0250dd7a081d0295e9701d65d33071da
samuel-rodrigues <carlossamuel.rodrigues@gmail.com>
2020-04-28 22:55:25 -0300

    Configuracao inicial maps

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+import MapView, {Marker} from 'react-native-maps';
 
 function Dashboard({navigation}) {
   navigation.setOptions({
     headerTransparent: true,
     title: '',
   });
-  return <View />;
+
+  const [position, setPosition] = useState({
+    latitude: -3.7497319,
+    longitude: -38.5513689,
+    latitudeDelta: 0.0042,
+    longitudeDelta: 0.092,
+  });
+
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map} region={position} />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    height: '100%',
+    width: '100%',
+  },
+});
 
 export default Dashboard;
